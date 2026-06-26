70a3dbf3749ccd0c62b16b181d58f628bc62ee5f
seelnus <seelnus@users.noreply.github.com>
2026-06-26 11:19:35 +0800

    更新前端应用逻辑

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,8 +23,8 @@ import {
   PointerSensor,
   useSensor,
   useSensors,
-  DragEndEvent,
 } from '@dnd-kit/core';
+import type { DragEndEvent } from '@dnd-kit/core';
 import {
   SortableContext,
   useSortable,
